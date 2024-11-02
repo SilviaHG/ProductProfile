532e9eb5751b43edd805e8d79d16ec19d9d037be
SilviaHG <69175669+SilviaHG@users.noreply.github.com>
2024-11-01 19:54:38 -0600

    Agregar la clase de ProductDetail.razor.cs

---
+++ ProductProfile/Components/Pages/ProductDetails.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProductProfile.Components.Models;
+
+namespace ProductProfile.Components.Pages
+{
+    public partial class ProductDetails: ComponentBase
+    {
+        [Parameter]
+        public Product product { get; set; }
+    }
+}
