e70d1fbb3b6db8fe2506cca0ce23766aaf20c333
SilviaHG <69175669+SilviaHG@users.noreply.github.com>
2024-11-01 19:57:24 -0600

    Se agregó la clase de ProductPprofile.razor.cs

---
+++ ProductProfile/Components/Pages/ProductPprofile.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProductProfile.Components.Models;
+
+namespace ProductProfile.Components.Pages
+{
+    public partial class ProductPprofile: ComponentBase
+    {
+        private Product product;
+        public int idProduct = 5;
+        protected override void OnInitialized()
+        {
+            product = productR.GetProductbyId(idProduct);
+            Console.WriteLine(product != null ? "Usuario obtenido correctamente" : "No se obtuvo ningún usuario");
+
+        }
+    }
+}
